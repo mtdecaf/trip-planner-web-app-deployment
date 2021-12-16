eab6b8d83919c96cfa042480ca098f5890ae7ccc
Jacky Cao <jcvan0116@gmail.com>
2021-12-16 15:31:55 -0800

    Revert "hot-fix: changed DeleteTripRoute.js rout to delete controller capitalization"
    This reverts commit 3639fb4e662c5b9602053ca09bd8e32d8f8e06ee.

--- server/routes/DeleteTripRoute.js
+++ server/routes/DeleteTripRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authorize = require("../middlewares/auth");
-const { deleteTripController } = require('../controllers/tripController');
+const { deleteTripController } = require('../controllers/TripController');
 
 router.delete('/:tripId', authorize, deleteTripController);
 
