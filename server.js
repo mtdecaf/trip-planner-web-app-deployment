eef305d47f970c1c708cab5ad8047d91ca06e6ae
Jacky Cao <jcvan0116@gmail.com>
2021-12-16 19:32:15 -0800

    package.json modification

--- server.js
+++ server.js
@@ -64,10 +64,10 @@ process.env.NODE_ENV = 'production';
 console.log(__dirname);
 console.log(process.env.NODE_ENV);
 // if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../build')))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../build'))
-    })
+    app.use(express.static(path.join(__dirname, "client", "build")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
 // }
 
 app.listen({port}, () => {
